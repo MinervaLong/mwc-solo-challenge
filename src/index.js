99f7a1e06b442777d87fd26306ec47a301f6c4a7
MinervaLong <vanesa.perea.cuevas@gmail.com>
2022-02-09 15:23:48 +0100

    Install  tailwind correctly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import './tailwind.css';
 import './styles/globals.css';
 import App from './App';
 
